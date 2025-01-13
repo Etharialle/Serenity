1144f0571a573d63efe407298cad30af7fabeb5c
etharialle <christopher.m.adams2@gmail.com>
2025-01-13 09:13:31 -0800

    clean slate

--- src/overworld_generator/src/include/create_map_png.h
+++
@@ -1,8 +0,0 @@
-#ifndef CREATE_MAP_PNG_H
-#define CREATE_MAP_PNG_H
-
-#include <vector>
-
-void createPNGFromArray(const std::vector<std::vector<int>>& map, const char* filename);
-
-#endif
--- src/overworld_generator/src/include/drunkards_walk.h
+++
@@ -1,9 +0,0 @@
-#ifndef DRUNKARDS_WALK_H
-#define DRUNKARDS_WALK_H
-
-#include <vector>
-
-void drunkardsWalk(std::vector<std::vector<int>>& map, int startX, int startY, int steps);
-void displayMap(const std::vector<std::vector<int>>& map);
-
-#endif
